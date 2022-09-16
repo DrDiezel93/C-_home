f5c7720de4b86dbaeb0b079144d24ebcf21cac14
Dr.Ddiezel93 <ghek93@mail.ru>
2022-09-16 18:43:25 +0300

    Добавил решение задачи 2

--- TaskHome02/Program.cs
+++ TaskHome02/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write ("Введите 1-е целое число: ");
+int numb1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите 2-е целое число: ");
+int numb2 = Convert.ToInt32 (Console.ReadLine());
+if (numb1 == numb2)
+{
+	Console.Write ("Числа равны");
+}
+if (numb1 > numb2)
+{
+	Console.Write ($"Число {numb1} большее, а число {numb2} меньшее");
+}
+if (numb1 < numb2)
+{
+	Console.Write ($"Число {numb2} большее, а число {numb1} меньшее");
+}
