3f5adb25511b00b70b9ee146fe54a5cadacab7c1
Dr.Ddiezel93 <ghek93@mail.ru>
2022-09-16 21:04:59 +0300

    Добавил решение задачи 8

--- TaskHome08/Program.cs
+++ TaskHome08/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write ("Введите целое положительное ненулевое число: ");
+int numb = Convert.ToInt32 (Console.ReadLine());
+if (numb <= 1)
+{
+Console.Write ("Вы ввели неверное число");
+}
+else
+{	int count = 2;
+	while (count <= numb)
+	{
+	Console.Write ($"{count} ");
+	count = count + 2;
+	}	 
+}
