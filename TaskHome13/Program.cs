15fa0b121f265031e194ec2c58316c1310d4303d
Dr.Ddiezel93 <ghek93@mail.ru>
2022-09-22 20:17:54 +0300

    Обновил решение задачи 13

--- TaskHome13/Program.cs
+++ TaskHome13/Program.cs
@@ -13,18 +13,18 @@ int ThirdNumb (int num)
    }
    return result;
 }
-bool Third2 (int num)
+bool NumberLess100 (int num)
 {
     return num < 100;
 }
-bool Third1 (int num)
+bool NegativeNumber (int num)
 {
     return num < 0;
 }
-if (Third1 (numb)) Console.WriteLine ("Вы ввели отрицательное число");
-else if (Third2 (numb)) Console.WriteLine ("В веденном числе 3-й цифры нет");
+if (NegativeNumber (numb)) Console.WriteLine ("Вы ввели отрицательное число");
+else if (NumberLess100 (numb)) Console.WriteLine ("В веденном числе 3-й цифры нет");
 else 
 {
-    int Result = ThirdNumb (numb);
-    Console.WriteLine ($"Третья цифра в веденном числе: {Result}");
+    int result2 = ThirdNumb (numb);
+    Console.WriteLine ($"Третья цифра в веденном числе: {result2}");
 }
