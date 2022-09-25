fe7f44d016e9f49b91e23629cc78da1e041b581f
Dr.Ddiezel93 <ghek93@mail.ru>
2022-09-25 11:05:32 +0300

    Добавил решение задач 21, 23. 19 задача под вопросом

---
+++ TaskHome21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки А");
+Console.Write("Xа: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Yа: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Zа: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+Console.Write("Xb: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Yb: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Zb: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xac, int yac, int zac, int xbc, int ybc, int zbc)
+{
+    double result = Math.Sqrt((xbc - xac) * (xbc - xac) + (ybc - yac) * (ybc - yac) + (zbc - zac)*(zbc - zac));
+    return result;
+}
+    double distance = Distance(xA, yA, zA, xB, yB, zB);
+    distance = Math.Round(distance, 2, MidpointRounding.ToZero);
+    Console.Write($"Расстояние между точками: {distance.ToString(System.Globalization.CultureInfo.GetCultureInfo("en-US"))}");
---
+++ TaskHome23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void numberСube (int numb)
+{
+    int i = 1;
+    while (i <= numb - 1)
+    {
+        int num = i * i * i;
+        Console.Write($"{num}, ");
+        i++;
+    }
+    int lastNumb = numb * numb * numb; Console.Write($"{lastNumb} ");
+}
+numberСube (number);
