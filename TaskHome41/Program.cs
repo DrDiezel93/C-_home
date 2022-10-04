7f54a5c4e52c400a3b4e271531e644bf7cf83e52
Dr.Ddiezel93 <ghek93@mail.ru>
2022-10-04 22:04:43 +0300

    Добавил решение задач 41 и 43

---
+++ TaskHome41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите общее колличество целых чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+if (size > 0)
+{
+    NumberPositiveNumbers(size);
+
+    void NumberPositiveNumbers(int siz)
+    {
+        int count = default;
+        for (int i = 1; i <= siz; i++)
+        {
+            Console.Write($"Введите {i}-е число: ");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            if (numb > 0) count++;
+        }
+        if (count > 0) Console.WriteLine($"Вы ввели {count} положительных числа");
+        else Console.WriteLine("Вы не ввели положительных чисел");
+    }
+}
+else Console.WriteLine("Вы ввели некорректное значение");
---
+++ TaskHome43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write ("Введите значение свободного коэффициента 1-й прямой: b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите значение углового коэффициента 1-й прямой: к1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите значение свободного коэффициента 2-й прямой: b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите значение углового коэффициента 2-й прямой: к2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2) Console.WriteLine ("Прямые не пересакаются т.к. они параллельные");
+else
+{
+    double x = CoordinatesX (k1, b1, k2, b2);
+    double y = CoordinatesY (k1, b1, k2, b2);
+    Console.WriteLine ($"Координаты точки пересчения прямых: ({x,1}; {y,1})");
+}
+double CoordinatesX (double k1c, double b1c, double k2c, double b2c)
+{
+    double xc = (b2c - b1c)/(k1c - k2c);
+    return xc;
+}
+double CoordinatesY (double k1c, double b1c, double k2c, double b2c)
+{
+    double yc = (k1c*b2c - k2c*b1c)/(k1c - k2c);
+    return yc;
+}
