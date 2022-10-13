a16d1e83d87d492bea8cf92d44326c532945e820
Dr.Ddiezel93 <ghek93@mail.ru>
2022-10-14 00:04:45 +0300

    Добавил решение задач 64, 66, 68

---
+++ TaskHome66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M:  ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N:  ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumElementsRange (numberM, numberN);
+Console.Write(sum);
+
+int SumElementsRange (int numb1, int numb2)
+{
+    int sum = numb1;
+    if (numb1 < numb2)
+    {
+        sum = sum + SumElementsRange (numb1+1, numb2);
+    }
+    if (numb1 > numb2)
+    {
+        sum = sum + SumElementsRange (numb1-1, numb2);
+    }
+    return sum;
+}
---
+++ TaskHome68/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int functionAckermann = FunctionAckermann (2, 3);
+Console.WriteLine(functionAckermann);
+
+int FunctionAckermann (int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAckermann (m - 1, 1);
+    return FunctionAckermann (m - 1, FunctionAckermann (m, n-1));
+}
